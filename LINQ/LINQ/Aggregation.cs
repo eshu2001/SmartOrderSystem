956e1489a3a33d66b5a9e54ce7898a7ae1f6d5d3
deshita2001@gmail.com <deshita2001@gmail.com>
2026-03-24 21:25:58 -0400

    LINQ

---
+++ LINQ/LINQ/Aggregation.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Linq;
+
+//public class Aggregation
+//{
+//    static void Main(string[] argrs)
+//    {
+//        List<Student> students = new List<Student>()
+//    {
+//        new Student{Id=1,Name="Arjun",Age=25, Marks= 80},
+//        new Student { Id = 2, Name = "Sara", Age = 22, Marks = 70 },
+//        new Student { Id = 3, Name = "Mike", Age = 21, Marks = 90 },
+//        new Student { Id = 4, Name = "Emma", Age = 23, Marks = 60 },
+//        new Student { Id = 5, Name = "David", Age = 20, Marks = 75 }
+//    };
+//        var Tstudents = students.Count();
+//        var Gmarks = students.Where(l => l.Marks > 75).Count();
+//        var Sum = students.Sum(l => l.Marks);
+//        var Average = students.Average(l => l.Age);
+//        var Maxmarks = students.Max(l => l.Marks);
+//        var Minmarks = students.Min(l => l.Marks);
+
+//        Console.WriteLine("Total number of students: " + Tstudents);
+//        Console.WriteLine("No of students got more than 75marks: " + Gmarks);
+//        Console.WriteLine("Sum of marks: " + Sum);
+//        Console.WriteLine("Average: " + Average);
+//        Console.WriteLine("Max Marks: " + Maxmarks);
+//        Console.WriteLine("Min Marks: " + Minmarks);
+//    }
+
+
+//}
+
+//public class Student
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+//    public int Marks { get; set; }
+//}
+
---
+++ LINQ/LINQ/Join.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Score
+{
+    public int StudentId { get; set; }
+    public int Marks { get; set; }
+}
+
+public class StudentScore
+{
+    public string Name { get; set; }
+    public int Marks { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1, Name="Arjun", Age=25},
+            new Student{Id=2, Name="Sara", Age=22},
+            new Student{Id=3, Name="Mike", Age=21},
+            new Student{Id=4, Name="Emma", Age=23},
+            new Student{Id=5, Name="David", Age=24} 
+        };
+
+        
+        List<Score> scores = new List<Score>()
+        {
+            new Score{StudentId=1, Marks=80},
+            new Score{StudentId=2, Marks=70},
+            new Score{StudentId=3, Marks=90},
+            new Score{StudentId=4, Marks=60}
+        };
+
+        //Inner Join: Student Name + Marks
+        var innerJoin = students.Join(
+            scores,
+            s => s.Id,
+            sc => sc.StudentId,
+            (s, sc) => new { s.Name, sc.Marks }
+        );
+
+        Console.WriteLine("----- Inner Join (Students with Scores) -----");
+        foreach (var item in innerJoin)
+        {
+            Console.WriteLine($"Name: {item.Name}, Marks: {item.Marks}");
+        }
+
+
+    }
+}
